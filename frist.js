ea0eafe29ab9a3d0a44710f39191bb9430d0fefe
adhishree03 <146438711+adhishree03@users.noreply.github.com>
2025-07-09 08:44:49 +0530

    Var || Let || Const

---
+++ frist.js
@@ -0,0 +1,66 @@
+//variable
+// Re-assign
+//Kahi bhi access karlo
+
+var age =22;
+var num1=11;
+var num2=16;
+
+console.log(num1 + num2);
+
+// Naming Conv
+// firstname
+// first_name
+// FirstName
+// firstName(Pref) Camel Casing
+
+// Var-> Global
+// Let & Const are LOCAL
+
+//eg-VAR
+if(true){
+    var age=23
+}
+console.log(age);
+
+
+
+//Let- Local
+//Re-assign
+//Access sirg khud ke block ke andr
+
+let age=23;
+
+age ="ADI";
+
+console.log(age);
+
+
+
+//const
+// Scope => local
+//Re-assign ni kar skte
+// Access sirf kud ke block ke andar
+
+const age =23;
+
+console.log(age + 10);
+
+//eg(ERROR CASE)
+
+/*
+   if(true)
+   {
+   const a=10;
+}
+   console.log(a);
+   
+   (CORRECT FORM)
+
+   if(true)
+   {
+   const a=10;
+    console.log(a);
+}
+  */
+

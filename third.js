14fcb1895500bf715642dcff12d211af2a24b97f
adhishree03 <146438711+adhishree03@users.noreply.github.com>
2025-07-10 02:50:40 +0530

    Conditional || Switch || Ternary

---
+++ third.js
@@ -0,0 +1,47 @@
+//CONDITIONAL STATEMENTS
+
+/*const age = 85;
+
+ if(age>=80){
+    console.log("Yes, You are an adult");
+}
+else if(age>=18){
+    console.log("You can Vote");
+}
+else{
+    console.log("No,You cant");
+}
+
+
+//Ternary Operators
+
+const age =90;
+
+age>=18 ? console.log('Yes') : console.log('No')*/
+
+
+//SWITCH CASES
+const option = 4;
+
+switch(option){
+    case 1:{
+        console.log('Namaste')
+    }
+    break;
+    case 2:{
+        console.log('Hello')
+    }
+    break;
+    case 3:{
+        console.log('Bonjour!')
+    }
+    break;
+    default: {
+        console.log('InValid Option')
+    }
+
+}
+
+
+
+

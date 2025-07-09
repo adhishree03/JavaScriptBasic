db859e36d667890e5140af698c77c0c2a05ffa26
adhishree03 <146438711+adhishree03@users.noreply.github.com>
2025-07-10 02:29:25 +0530

    DataTypes || Operators&Types

---
+++ second.js
@@ -0,0 +1,69 @@
+// DataTypes
+
+// NUMBERS
+
+let num =3.12;
+let num2= 12;
+console.log(num+num2);
+
+// STRINGS
+
+// can use single quote or double acc to wish
+const firstName = ' Adi';
+const lastName = "Srivastava";
+console.log(firstName + ' ' + lastName);
+
+
+// BOOLEANS
+
+let isLoggedIn = true;
+
+console.log(isLoggedIn)
+
+
+// NULL
+
+ let lastLoginDate = null;
+
+ console.log(lastLoginDate);
+
+
+ // OBJECTS
+
+ const person ={
+    firstName :'ADI',
+    lastName : 'Srivastava',
+    age : 22 ,
+    isLoggenIn : false,
+    lastLoginDate : null
+ }
+ const person2 ={
+    firstName :'Aru',
+    lastName : 'Srivastava',
+    age : 26 ,
+
+ }
+ console.log(person.firstName)
+
+
+ //JS 1 -> TRUE | 0 -> FALSE
+ // JS TRUE | FALSE
+
+ // true -> 1
+ // false -> 0
+
+ //OPERATOR
+ // + , - , * , / 
+
+
+ //string '1' + '1' -> 11
+ //string +number =string
+ //string * num = number
+
+
+ //TypeOfOperator
+console.log(typeof '123'); //->string
+console.log(typeof ('123'*1)); //->number
+console.log(typeof lastLoginDate); //-> object
+ console.log(typeof person); //->object
+  console.log(typeof num1); //-> number

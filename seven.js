3a6365e800648e5a3ca91778bd825ce73f778f90
adhishree03 <146438711+adhishree03@users.noreply.github.com>
2025-07-13 09:40:31 +0530

    arrow function

---
+++ seven.js
@@ -0,0 +1,45 @@
+//ARROW FUNCTION
+
+// 1) Syntax
+
+/*function sayHello(){
+    console.log("Hello!");
+
+    sayHello()
+}
+// Above code in arrow function
+const sayHelo = () =>{   //ARROW FUNCTION
+    console.log("HEY!!")
+};
+//const add = (a,b) =>{
+   // return a+b; };
+const addV2 = (a,b) => a+b; //one liner
+
+console.log( addV2(2,3));    
+
+
+
+
+// 2) 'arguments' keyword
+
+const addNumbers =(...nums) =>  //spread operation
+{
+    console.log(nums);
+}
+addNumbers(10,22,3,23,4);*/
+
+
+
+
+// 3) hoisting
+
+sayHey();
+
+function sayHey() 
+{
+    console.log("Hey there");
+}
+
+
+
+// 4) This Keyword
